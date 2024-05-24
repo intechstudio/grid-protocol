251e57ffaa9a27c0f5dd8d42810ad90ac2ea67c8
SukuWc <32985513+SukuWc@users.noreply.github.com>
2024-05-24 14:19:03 +0200

    SUKU sync

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        library: 'MyLibrary',
+        libraryTarget: 'umd',
+        globalObject: 'this'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    externals: {
+        // Specify any dependencies you don't want to bundle here.
+        // For example: 'react': 'React'
+    }
+};
